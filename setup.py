5cd379d108a4732a43ee2cd450e2a884dfdc62ff
qimiko <25387744+qimiko@users.noreply.github.com>
2022-03-24 10:15:19 +0000

    fix setup indentation

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ Setup script for pip
 from setuptools import setup, find_packages
 
 setup(
-	name='gdlevelconverter',
-	version='1.0',
-	packages=find_packages(),
-	entry_points = {
-		'console_scripts': ['gd-level-converter=gdlevelconverter.command_line:_main']
-	},
-	author='zmx',
-	description='2.0+ to 1.9 Geometry Dash level conversion tool',
-	install_requires=[
-		"setuptools"
-	],
+    name='gdlevelconverter',
+    version='1.0',
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['gd-level-converter=gdlevelconverter.command_line:_main']
+    },
+    author='zmx',
+    description='2.0+ to 1.9 Geometry Dash level conversion tool',
+    install_requires=[
+        "setuptools"
+    ],
 )
